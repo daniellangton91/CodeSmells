a60b2f28a58def6273855d665b02d0277bfceef2
Daniel <daniel.langton@iths.se>
2023-09-01 10:56:42 +0200

    Changed name on PlayerData class and moved to seperate file

---
+++ CodeSmells/Player.cs
@@ -0,0 +1,40 @@
+﻿namespace CodeSmells
+{
+    public class Player
+    {
+        public string Name { get; private set; }
+        public int NGames { get; private set; }
+        int totalGuess;
+
+
+        public Player(string name, int guesses)
+        {
+            this.Name = name;
+            NGames = 1;
+            totalGuess = guesses;
+        }
+
+        public void Update(int guesses)
+        {
+            totalGuess += guesses;
+            NGames++;
+        }
+
+        public double Average()
+        {
+            return (double)totalGuess / NGames;
+        }
+
+
+        public override bool Equals(Object p)
+        {
+            return Name.Equals(((Player)p).Name);
+        }
+
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}

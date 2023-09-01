a47137bac75c5ae42ee783960167f357b5782e9f
Daniel <daniel.langton@iths.se>
2023-09-01 13:46:13 +0200

    Created two implementations of IGame

---
+++ CodeSmells/Mastermind.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeSmells
+{
+    internal class Mastermind : IGame
+    {
+        public void CheckCorrectAnswer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateRandomNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

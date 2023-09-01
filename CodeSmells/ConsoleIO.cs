23af6386a2faa7446978a71ba127809818444d6a
Daniel <daniel.langton@iths.se>
2023-09-01 13:37:48 +0200

    Added interface for UI and created a class from it

---
+++ CodeSmells/ConsoleIO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSmells
+{
+    internal class ConsoleIO : IUI
+    {
+        public void Clear()
+        {
+            Console.Clear();
+        }
+
+        public void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        public string GetString()
+        {
+            return Console.ReadLine();
+        }
+
+        public void PutString(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ CodeSmells/IUI.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeSmells
+{
+    internal interface IUI
+    {
+        void PutString(string input);
+        string GetString();
+        void Clear();
+        void Exit();
+    }
+}

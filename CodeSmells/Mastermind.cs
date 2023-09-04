74641889db4780dd9d662317b2a5cb39919ce223
Daniel <daniel.langton@iths.se>
2023-09-04 12:54:23 +0200

    Created new implementation of IGame

--- CodeSmells/Mastermind.cs
+++ CodeSmells/Mastermind.cs
@@ -3,23 +3,98 @@
     internal class Mastermind : IGame
     {
         private IUI uI;
-        public Mastermind(IUI ui)
+        private IFileHandler storage;
+        private int numberOfGuesses;
+        private string guess;
+        private bool keepPlaying = true;
+        private string goal;
+        private string bullsAndCowsResult;
+        public Mastermind(IUI ui, IFileHandler storage)
         {
             this.uI = ui;
+            this.storage = storage;
         }
         public void CheckCorrectAnswer()
         {
-            throw new NotImplementedException();
+            do
+            {
+                guess = uI.GetString();
+                bullsAndCowsResult = CompareGuessToGoal(goal, guess);
+                uI.PutString($"{bullsAndCowsResult}");
+                numberOfGuesses++;
+            } while (guess != goal);
         }
 
         public string GenerateRandomNumber()
         {
-            throw new NotImplementedException();
+            Random randomGenerator = new Random();
+            string goal = "";
+            for (int i = 0; i < 4; i++)
+            {
+                int random = randomGenerator.Next(7);
+                goal = goal + random;
+            }
+            return goal;
         }
 
         public void PlayGame()
         {
-            throw new NotImplementedException();
+            uI.Clear();
+            uI.PutString("Mastermind\n");
+            uI.PutString("Enter your user name:");
+            Player player = new Player(uI.GetString());
+            do
+            {
+                player.UpdatePlayedGames();
+                numberOfGuesses = 0;
+                goal = GenerateRandomNumber();
+                PracticeGame();
+                uI.PutString("New game:");
+                CheckCorrectAnswer();
+                player.UpdateGuesses(numberOfGuesses);
+                storage.PutStatisticsToFile(player, "MastermindStats");
+                storage.DisplayPlayerStatistics();
+                uI.PutString("Correct, it took " + numberOfGuesses + " guesses\n");
+                uI.PutString("Play a new game?");
+                string Answer = uI.GetString();
+                if (Answer != null && Answer != "" && Answer.Substring(0, 1) == "n")
+                {
+                    keepPlaying = false;
+                }
+                uI.Clear();
+            } while (keepPlaying == true);
+        }
+        private void PracticeGame()
+        {
+            uI.PutString("Do you want to practice y/n?");
+            string practiceAnswer = uI.GetString();
+            if (practiceAnswer == "y")
+            {
+                uI.PutString("For practice, number is: " + goal + "\n");
+            }
+        }
+        static string CompareGuessToGoal(string goal, string guess)
+        {
+            int cows = 0, bulls = 0;
+            guess += "    ";     // if player entered less than 4 chars
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (goal[i] == guess[j])
+                    {
+                        if (i == j)
+                        {
+                            bulls++;
+                        }
+                        else
+                        {
+                            cows++;
+                        }
+                    }
+                }
+            }
+            return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
         }
     }
 }

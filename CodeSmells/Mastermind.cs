48970d79c13a6efc5fefd89dce0af69d091d664d
Daniel <daniel.langton@iths.se>
2023-09-04 16:40:11 +0200

    Changed inheritance to Game instead of IGame

--- CodeSmells/Mastermind.cs
+++ CodeSmells/Mastermind.cs
@@ -1,31 +1,14 @@
 ﻿namespace CodeSmells
 {
-    internal class Mastermind : IGame
+    internal class Mastermind : Game
     {
-        private IUI uI;
-        private IFileHandler storage;
-        private int numberOfGuesses;
-        private string guess;
-        private bool keepPlaying = true;
-        private string goal;
-        private string bullsAndCowsResult;
         public Mastermind(IUI ui, IFileHandler storage)
         {
             this.uI = ui;
             this.storage = storage;
+            gameType = "Mastermind";
         }
-        public void CheckCorrectAnswer()
-        {
-            do
-            {
-                guess = uI.GetString();
-                bullsAndCowsResult = CompareGuessToGoal(goal, guess);
-                uI.PutString($"{bullsAndCowsResult}");
-                numberOfGuesses++;
-            } while (guess != goal);
-        }
-
-        public string GenerateRandomNumber()
+        public override string GenerateRandomNumber()
         {
             Random randomGenerator = new Random();
             string goal = "";
@@ -35,66 +18,6 @@
                 goal = goal + random;
             }
             return goal;
-        }
-
-        public void PlayGame()
-        {
-            uI.Clear();
-            uI.PutString("Mastermind\n");
-            uI.PutString("Enter your user name:");
-            Player player = new Player(uI.GetString());
-            do
-            {
-                player.UpdatePlayedGames();
-                numberOfGuesses = 0;
-                goal = GenerateRandomNumber();
-                PracticeGame();
-                uI.PutString("New game:");
-                CheckCorrectAnswer();
-                player.UpdateGuesses(numberOfGuesses);
-                storage.PutStatisticsToFile(player, "MastermindStats");
-                storage.DisplayPlayerStatistics();
-                uI.PutString("Correct, it took " + numberOfGuesses + " guesses\n");
-                uI.PutString("Play a new game?");
-                string Answer = uI.GetString();
-                if (Answer != null && Answer != "" && Answer.Substring(0, 1) == "n")
-                {
-                    keepPlaying = false;
-                }
-                uI.Clear();
-            } while (keepPlaying == true);
-        }
-        private void PracticeGame()
-        {
-            uI.PutString("Do you want to practice y/n?");
-            string practiceAnswer = uI.GetString();
-            if (practiceAnswer == "y")
-            {
-                uI.PutString("For practice, number is: " + goal + "\n");
-            }
-        }
-        static string CompareGuessToGoal(string goal, string guess)
-        {
-            int cows = 0, bulls = 0;
-            guess += "    ";     // if player entered less than 4 chars
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    if (goal[i] == guess[j])
-                    {
-                        if (i == j)
-                        {
-                            bulls++;
-                        }
-                        else
-                        {
-                            cows++;
-                        }
-                    }
-                }
-            }
-            return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
         }
     }
 }
--- CodeSmells/Moo.cs
+++ CodeSmells/Moo.cs
@@ -1,67 +1,14 @@
 ﻿namespace CodeSmells
 {
-    internal class Moo : IGame
+    internal class Moo : Game
     {
-        private IUI uI;
-        private IFileHandler storage;
-        private int numberOfGuesses;
-        private string guess;
-        private bool keepPlaying = true;
-        private string goal;
-        private string bullsAndCowsResult;
         public Moo(IUI ui, IFileHandler storage) 
         {
             this.uI = ui;
             this.storage = storage;
+            gameType = "Moo";
         }
-        public void PlayGame()
-        {
-            uI.Clear();
-            uI.PutString("MooGame\n");
-            uI.PutString("Enter your user name:");
-            Player player = new Player(uI.GetString());
-            do
-            {
-                player.UpdatePlayedGames();
-                numberOfGuesses = 0;
-                goal = GenerateRandomNumber();
-                PracticeGame();
-                uI.PutString("New game:");
-                CheckCorrectAnswer();
-                player.UpdateGuesses(numberOfGuesses);
-                storage.PutStatisticsToFile(player, "MooStats");
-                storage.DisplayPlayerStatistics();
-                uI.PutString("Correct, it took " + numberOfGuesses + " guesses\n");
-                uI.PutString("Play a new game?");
-                string Answer = uI.GetString();
-                if (Answer != null && Answer != "" && Answer.Substring(0, 1) == "n")
-                {
-                    keepPlaying = false;
-                }
-                uI.Clear();
-            } while (keepPlaying == true);
-        }
-        public void CheckCorrectAnswer()
-        {
-            do
-            {
-                guess = uI.GetString();
-                bullsAndCowsResult = CompareGuessToGoal(goal, guess);
-                uI.PutString($"{bullsAndCowsResult}");
-                numberOfGuesses++;
-            } while (guess != goal);
-        }
-        private void PracticeGame()
-        {
-            uI.PutString("Do you want to practice y/n?");
-            string practiceAnswer = uI.GetString();
-            if (practiceAnswer == "y")
-            {
-                uI.PutString("For practice, number is: " + goal + "\n");
-            }
-        }
-
-        public string GenerateRandomNumber()
+        public override string GenerateRandomNumber()
         {
             Random randomGenerator = new Random();
             string randomNumbers = "";
@@ -78,30 +25,5 @@
             }
             return randomNumbers;
         }
-
-
-        static string CompareGuessToGoal(string goal, string guess)
-        {
-            int cows = 0, bulls = 0;
-            guess += "    ";     // if player entered less than 4 chars
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    if (goal[i] == guess[j])
-                    {
-                        if (i == j)
-                        {
-                            bulls++;
-                        }
-                        else
-                        {
-                            cows++;
-                        }
-                    }
-                }
-            }
-            return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
-        }        
     }
 }

695d6b855ddb9cc803463c38ce1b3f94d1a36bd2
Daniel <daniel.langton@iths.se>
2023-09-05 14:53:17 +0200

    Added functionality to search list of players

--- CodeSmells/IDataHandler.cs
+++ CodeSmells/IDataHandler.cs
@@ -5,5 +5,6 @@
         void SaveStatistics(Player player, string fileName);
         void LoadStatistics(string fileName);
         void DisplayPlayerStatistics();
+        public Player CheckIfPlayerExists(string name);
     }
 }
